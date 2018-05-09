c0743e786563034d2e4f46426d14e5b53bd7761e
luzuoquan <luzuoquanflex@gmail.com>
2018-05-09 23:13:28 +0800

    update

--- build/development.js
+++ build/development.js
@@ -6,13 +6,13 @@ const config = require('./webpack.dev.config.js')
 const options = {
   contentBase: path.resolve('./'),
   hot: true,
-  host: '192.168.1.107'
+  host: '192.168.1.105'
 }
 
 webpackDevServer.addDevServerEntrypoints(config, options)
 const compiler = webpack(config)
 const server = new webpackDevServer(compiler, options)
 
-server.listen(8080, '192.168.1.107', () => {
+server.listen(8080, '192.168.1.105', () => {
   console.log('dev server listening on port 8080')
 });
